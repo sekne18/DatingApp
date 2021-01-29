e6cb1854e088f2d8c97abb4a9920e6ec8607ec1e
sekne18 <sekne50@gmail.com>
2021-01-29 15:22:38 +0100

    Added error handling

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -19,9 +19,7 @@ export class NavComponent implements OnInit {
   login(){
     this.accountService.login(this.model).subscribe(response => {
       this.router.navigateByUrl('/members');
-    }, error => {
-      this.toastr.error(error.error);
-    })
+    });
   }
 
   logout(){
